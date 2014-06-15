903895972bce7c9b25ee3fee220db3f6e8712a66
Andrew Griffin <andrew.griffin@zxb45.co.uk>
2014-06-15 21:44:03 +0100

    read me and updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# Golang azure example
+
+This is a simple example of a site that can be build on azure with the go tools installed on the website
--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-D:\home\bin\go\bin\go.exe build -o %DEPLOYMENT_TARGET%\my_app2.exe 
-xcopy d:\home\site\repository\templates %DEPLOYMENT_TARGET%\templates\ /Y
+rem build the exe
+D:\home\go\bin\go.exe build -o %DEPLOYMENT_TARGET%\my_app2.exe 
+
+rem copy the web config from deplopyment to the site
 xcopy d:\home\site\repository\web.config d:\home\site\wwwroot\web.config /Y
