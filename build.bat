e0b29114ce0fe2c6c9fa997775484f0846d60025
Andrew Griffin <andrew.griffin@zxb45.co.uk>
2014-06-15 22:06:37 +0100

    update batch file to use copy not xcopy.

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ rem build the exe
 D:\home\go\bin\go.exe build -o %DEPLOYMENT_TARGET%\my_app2.exe 
 
 rem copy the web config from deplopyment to the site
-xcopy d:\home\site\repository\web.config d:\home\site\wwwroot\web.config /Y
+copy d:\home\site\repository\web.config d:\home\site\wwwroot\web.config
